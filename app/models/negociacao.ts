a421f8f26d1d04be9a546b12ea1682abfd977fb1
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-09-24 19:59:24 -0300

    Programação defensiva. Protegendo o atributo data de Negociação.

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,9 +1,15 @@
 export class Negociacao {
     constructor(
-        public readonly data: Date, 
+        private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
+
+    get data(): Date {
+        //Retornando uma outra referência para data, impedindo que o atributo _data do meu objeto seja modificado em outro lugar do código.
+        const data = new Date(this._data.getTime()); 
+        return data;
+    }
 
     get volume(): number {
         return this.quantidade * this.valor;
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,8 +1,13 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this.data = data;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
+    }
+    get data() {
+        //Retornando uma outra referência para data, impedindo que o atributo _data do meu objeto seja modificado em outro lugar do código.
+        const data = new Date(this._data.getTime());
+        return data;
     }
     get volume() {
         return this.quantidade * this.valor;
