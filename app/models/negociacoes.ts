6965ee04d32b64af5f89b9d5a33dfe71c18cead0
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-09-30 17:43:31 -0300

    Atribuindo private e public nas funções e centralizando atualização de view

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -4,12 +4,12 @@ export class Negociacoes {
     // private negociacoes: Array<Negociacao> = []; 
     private negociacoes: Negociacao [] = []; // Simplicação da linha de cima, mas ambas compilam
 
-    adiciona(negociacao: Negociacao) {
+    public adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
     // lista(): ReadonlyArray<Negociacao> {
-    lista(): readonly Negociacao[] { // Simplicação da linha de cima, mas ambas compilam
+    public lista(): readonly Negociacao[] { // Simplicação da linha de cima, mas ambas compilam
         return this.negociacoes;
     }
 }
