da4b46fb82b9156876281b3250d6c2f3ffbd85ee
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-10-01 19:37:55 -0300

    Criando atributos opcionais

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,13 +1,21 @@
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
+    private escapar = false;
 
-    constructor(seletor: string) {
+    //escapar é um parâmetro opcional. Parâmetros Opcionais devem vir por último no construtor.
+    constructor(seletor: string, escapar?: boolean) {
         this.elemento = document.querySelector(seletor);
+        if(escapar){
+            this.escapar = escapar;
+        }
     }
 
     public update(model: T): void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if(this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, ''); //Remove os scripts dentro dos templates
+        }
         this.elemento.innerHTML = template;
     }
 
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,9 +1,17 @@
 export class View {
-    constructor(seletor) {
+    //escapar é um parâmetro opcional. Parâmetros Opcionais devem vir por último no construtor.
+    constructor(seletor, escapar) {
+        this.escapar = false;
         this.elemento = document.querySelector(seletor);
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, ''); //Remove os scripts dentro dos templates
+        }
         this.elemento.innerHTML = template;
     }
 }
