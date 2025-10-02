0ac68f834ba26b6a5c8cae21f4d284ad4373b065
Marcos Laerte Gomes de Lima <marcos.laerte.lima@gmail.com>
2025-10-02 17:56:40 -0300

    Verificando variáveis que podem ser nulas

--- app/views/view.ts
+++ app/views/view.ts
@@ -5,15 +5,20 @@ export abstract class View<T> {
 
     //escapar é um parâmetro opcional. Parâmetros Opcionais devem vir por último no construtor.
     constructor(seletor: string, escapar?: boolean) {
-        this.elemento = document.querySelector(seletor);
-        if(escapar){
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique!`);
+        }
+        if (escapar) {
             this.escapar = escapar;
         }
     }
 
     public update(model: T): void {
         let template = this.template(model);
-        if(this.escapar) {
+        if (this.escapar) {
             template = template.replace(/<script>[\s\S]*?<\/script>/, ''); //Remove os scripts dentro dos templates
         }
         this.elemento.innerHTML = template;
